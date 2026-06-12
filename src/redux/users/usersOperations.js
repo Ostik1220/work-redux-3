25c8799f15788d8bb90fc937e5308bca57a32ee9
Ostik <117195748+Ostik1220@users.noreply.github.com>
2026-06-12 23:07:02 +0300

    save

--- src/redux/users/usersOperations.js
+++ src/redux/users/usersOperations.js
@@ -1,20 +1,18 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-
 export const createUser = createAsyncThunk(
-    "users/createUser",
-    async(obj, thunkAPI) => {
-        try{
-            const response = await axios.post("http://localhost:3001/register", {
-                email: obj.email,
-                password: obj.password
-            })
-            const data = response.data;
-            console.log(data)
-            return data
-            
-        } catch(error){
-            return thunkAPI.rejectWithValue(error.message);
-        }
+  "users/createUser",
+  async (obj, thunkAPI) => {
+    try {
+      const response = await axios.post("http://localhost:3001/register", {
+        email: obj.email,
+        password: obj.password,
+      });
+      const data = response.data;
+      console.log(data);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
     }
-)
+  }
+);
