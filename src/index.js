4ec5ff239933f99cb0283feacfd3fc8fe55f7401
Ostik <117195748+Ostik1220@users.noreply.github.com>
2026-06-21 18:03:32 +0300

    save

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-    <BrowserRouter>
+    <BrowserRouter basename='/work-redux-3'>
      <App />
    </BrowserRouter>    </PersistGate>
   </Provider>
